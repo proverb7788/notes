0ee245d901f4903f644bc3bae0117d11bdb6694c
l334964413 <l334964413@sina.com>
2018-01-11 13:32:30 +0800

    test

---
+++ test/src/com/test/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright(c) 2015-2018 by XinSheng Inc.
+ * All Rights Reserved
+ */
+package com.test;
+/**
+ * @author Qiyang.Zhang
+ * 2018年1月11日 下午1:31:38
+ */
+public class Test {
+
+}
